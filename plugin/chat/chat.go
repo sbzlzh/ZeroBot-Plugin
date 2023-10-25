cbca53f0a859a8bdd61a1ef7ebdf4a292dc079fd
波と一緒に漂う <2247564152@qq.com>
2023-10-25 15:49:05 +0800

    Update chat.go

--- plugin/chat/chat.go
+++ plugin/chat/chat.go
@@ -45,11 +45,11 @@ func init() { // 插件主体
 			case poke.Load(ctx.Event.GroupID).AcquireN(3):
 				// 5分钟共8块命令牌 一次消耗3块命令牌
 				time.Sleep(time.Second * 1)
-				ctx.SendChain(message.Text("请不要戳", nickname, " >_<"))
+				ctx.SendChain(message.Text("请不要戳我啦，你是不是要把我吃掉哇。",  " >_<"))
 			case poke.Load(ctx.Event.GroupID).Acquire():
 				// 5分钟共8块命令牌 一次消耗1块命令牌
 				time.Sleep(time.Second * 1)
-				ctx.SendChain(message.Text("喂(#`O′) 戳", nickname, "干嘛！"))
+				ctx.SendChain(message.Text("喂(#`O′) 戳我",  "干嘛！你是不是要吃掉我。>_<"))
 			default:
 				// 频繁触发，不回复
 			}
